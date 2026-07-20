9383dd6b3507377f35372fe27c16bae035a3e1ba
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2026-07-20 11:53:26 +0200

    file input support

--- src/console.cc
+++ src/console.cc
@@ -58,6 +58,45 @@ namespace mttlib {
     }
   }
 
+  ConsoleResult Console::ReadLine(WideString &string) noexcept {
+    string.Clear();
+
+    wchar_t buffer[kBufferSize];
+
+    while (true) {
+      DWORD bytes_read;
+
+      if (ReadConsoleW(input_, buffer, kBufferSize, &bytes_read, NULL) == 0) {
+        return ConsoleResult::kSystemFailure;
+      }
+
+      if (bytes_read == 0) {
+        return ConsoleResult::kAborted;
+      }
+
+      int ignored = 0;
+
+      if (buffer[bytes_read - 1] == '\r') {
+        ++ignored;
+      }
+      else if (buffer[bytes_read - 1] == '\n') {
+        ++ignored;
+
+        if (bytes_read > 1 && buffer[bytes_read - 2] == '\r') {
+          ++ignored;
+        }
+      }
+
+      if (string.Append(buffer, bytes_read - ignored) == false) {
+        return ConsoleResult::kMemoryAllocationFailure;
+      }
+
+      if (ignored != 0) {
+        return ConsoleResult::kSuccess;
+      }
+    }
+  }
+
   int Console::Write(CharactersSpan characters_span, bool append_line_feed)
       const noexcept {
     DWORD bytes_written;
@@ -68,6 +107,22 @@ namespace mttlib {
     }
 
     if (append_line_feed && WriteConsoleA(output_, kLineFeed, 1, NULL, NULL) != 0) {
+      ++bytes_written;
+    }
+
+    return bytes_written;
+  }
+
+  int Console::Write(WideCharactersSpan characters_span, bool append_line_feed)
+      const noexcept {
+    DWORD bytes_written;
+
+    if (WriteConsoleW(output_, characters_span.pointer(), characters_span.size(),
+        &bytes_written, NULL) == 0) {
+      return -1;
+    }
+
+    if (append_line_feed && WriteConsoleW(output_, kWideLineFeed, 1, NULL, NULL) != 0) {
       ++bytes_written;
     }
 
---
+++ src/file_input_stream.cc
@@ -0,0 +1,179 @@
+#include "file_input_stream.h"
+
+#include <Windows.h>
+
+#include <cassert>
+#include <cstring>
+#include <new>
+
+namespace mttlib {
+  void FileInputStream::Destroy() noexcept {
+    CloseHandle(handle_);
+    delete[] buffer_;
+  }
+
+  std::optional < FileInputStream > FileInputStream::Construct(const char *path)
+      noexcept {
+    assert(path != nullptr);
+
+    char *buffer = new(std::nothrow) char[kBufferSize];
+
+    if (buffer == nullptr) {
+      return std::nullopt;
+    }
+
+    HANDLE handle = CreateFileA(path, GENERIC_READ, 0,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (handle == INVALID_HANDLE_VALUE) {
+      delete[] buffer;
+
+      return std::nullopt;
+    }
+
+    return FileInputStream(handle, buffer);
+  }
+
+  std::optional < FileInputStream > FileInputStream::Construct(const wchar_t *path)
+      noexcept {
+    assert(path != nullptr);
+
+    char *buffer = new(std::nothrow) char[kBufferSize];
+
+    if (buffer == nullptr) {
+      return std::nullopt;
+    }
+
+    HANDLE handle = CreateFileW(path, GENERIC_READ, 0,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (handle == INVALID_HANDLE_VALUE) {
+      delete[] buffer;
+
+      return std::nullopt;
+    }
+
+    return FileInputStream(handle, buffer);
+  }
+
+  FileInputStream::FileInputStream(FileInputStream &&other) noexcept {
+    handle_ = other.handle_;
+    handle_offset_ = other.handle_offset_;
+    buffer_ = other.buffer_;
+    buffer_offset_ = other.buffer_offset_;
+    valid_count_ = other.valid_count_;
+    other.handle_ = INVALID_HANDLE_VALUE;
+    other.buffer_ = nullptr;
+  }
+
+  FileInputStream &FileInputStream::operator = (FileInputStream &&other) noexcept {
+    if (this != &other) {
+      Destroy();
+      handle_ = other.handle_;
+      handle_offset_ = other.handle_offset_;
+      buffer_ = other.buffer_;
+      buffer_offset_ = other.buffer_offset_;
+      valid_count_ = other.valid_count_;
+      other.handle_ = INVALID_HANDLE_VALUE;
+      other.buffer_ = nullptr;
+    }
+
+    return *this;
+  }
+
+  int FileInputStream::Read(char *buffer, int count) noexcept {
+    assert(buffer != nullptr && count >= 0);
+
+    if (count <= valid_count_) {
+      std::memcpy(buffer, buffer_ + buffer_offset_, count);
+      buffer_offset_ += count;
+      valid_count_ -= count;
+
+      return count;
+    }
+
+    int cumulative_count = valid_count_;
+
+    std::memcpy(buffer, buffer_ + buffer_offset_, valid_count_);
+    buffer_offset_ = 0;
+    valid_count_ = 0;
+
+    while (true) {
+      DWORD bytes_read;
+
+      if (count <= cumulative_count + kBufferSize) {
+        if (ReadFile(handle_, buffer_, kBufferSize, &bytes_read, NULL) == 0 ||
+            bytes_read == 0) {
+          return cumulative_count;
+        }
+
+        handle_offset_ += bytes_read;
+
+        if (count <= static_cast < int > (cumulative_count + bytes_read)) {
+          int remainder = count - cumulative_count;
+
+          std::memcpy(buffer + cumulative_count, buffer_, remainder);
+          buffer_offset_ = remainder;
+          valid_count_ = bytes_read - remainder;
+
+          return count;
+        }
+        else { // not enough data
+          std::memcpy(buffer + cumulative_count, buffer_, bytes_read);
+          cumulative_count += bytes_read;
+        }
+      }
+      else { // direct copy to destination buffer
+        if (ReadFile(handle_, buffer + cumulative_count, kBufferSize,
+            &bytes_read, NULL) == 0 || bytes_read == 0) {
+          return cumulative_count;
+        }
+
+        handle_offset_ += bytes_read;
+        cumulative_count += bytes_read;
+      }
+    }
+  }
+
+  bool FileInputStream::Seek(std::int64_t offset, SeekPosition position) noexcept {
+    std::int64_t real_offset;
+
+    if (position == SeekPosition::kBeginning) {
+      real_offset = 0;
+    }
+    else if (position == SeekPosition::kEnd) {
+      LARGE_INTEGER file_size;
+
+      if (GetFileSizeEx(handle_, &file_size) == 0) {
+        return false;
+      }
+
+      real_offset = file_size.QuadPart;
+    }
+    else {
+      real_offset = handle_offset_;
+    }
+
+    real_offset += offset;
+
+    std::int64_t first_offset = handle_offset_ - valid_count_ - buffer_offset_;
+
+    if (real_offset >= first_offset && real_offset <= handle_offset_) {
+      buffer_offset_ = static_cast < int > (real_offset - first_offset);
+      valid_count_ = static_cast < int > (handle_offset_ - real_offset);
+    }
+    else {
+      LARGE_INTEGER pointer{ .QuadPart = real_offset };
+
+      if (SetFilePointerEx(handle_, pointer, NULL, NULL) == 0) {
+        return false;
+      }
+
+      handle_offset_ = real_offset;
+      buffer_offset_ = 0;
+      valid_count_ = 0;
+    }
+
+    return true;
+  }
+}
